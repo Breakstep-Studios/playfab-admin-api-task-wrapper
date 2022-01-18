2e0d7f6a01fc2559f98798127fb5fae142fe2c47
Thomas <thsbrown1@gmail.com>
2022-01-18 12:46:15 -0800

    Initial Commit

---
+++ Runtime/PlayFabAdminAPIWrapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using PlayFab;
+using ThomasBrown.PlayFab;
+
+namespace BreakstepStudios.Scripts.Runtime.PlayFab
+{
+    public static class PlayFabAdminAPIWrapper
+    {
+
+    }
+}
