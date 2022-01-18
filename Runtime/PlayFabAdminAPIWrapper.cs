9c0436ef496fb285791fc323eda0d3fd42ebe8d6
Thomas <thsbrown1@gmail.com>
2022-01-18 12:59:25 -0800

    Add GetTitleDataAsync method

--- Runtime/PlayFabAdminAPIWrapper.cs
+++ Runtime/PlayFabAdminAPIWrapper.cs
@@ -1,11 +1,25 @@
 ﻿using System.Threading.Tasks;
 using PlayFab;
+using PlayFab.AdminModels;
 using ThomasBrown.PlayFab;
 
 namespace BreakstepStudios.Scripts.Runtime.PlayFab
 {
     public static class PlayFabAdminAPIWrapper
     {
-
+        /// <inheritdoc cref="PlayFabAdminAPI.GetTitleData"/>
+        public static Task<PlayFabCommonResponse<GetTitleDataResult>> GetTitleDataAsync(
+            GetTitleDataRequest request)
+        {
+            var taskCompletionSource = new TaskCompletionSource<PlayFabCommonResponse<GetTitleDataResult>>();
+            PlayFabAdminAPI.GetTitleData(request, (result) =>
+            {
+                taskCompletionSource.SetResult(new PlayFabCommonResponse<GetTitleDataResult>(result,null));
+            }, (error) =>
+            {
+                taskCompletionSource.SetResult(new PlayFabCommonResponse<GetTitleDataResult>(null, error));
+            });
+            return taskCompletionSource.Task;
+        }
     }
 }
